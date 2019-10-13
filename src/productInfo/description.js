47d571b34d5a0895abf8f84ee66ef92bd87240ca
time2begin <50028957+time2begin@users.noreply.github.com>
2019-10-13 17:06:13 +0300

    запихнули папку в гит

---
+++ src/productInfo/description.js
@@ -0,0 +1,71 @@
+import kolesaProm from "@/productInfo/kolesa/kolesaProm";
+import kolesaGidr from "@/productInfo/kolesa/kolesaGidr";
+import kolBolGruz from "@/productInfo/kolesa/kolBolGruz";
+import kolBezOpori from "@/productInfo/kolesa/kolBezOpori";
+import kolPnevmo from "@/productInfo/kolesa/kolPnevmo";
+import KolesaMusor from "@/productInfo/kolesa/KolesaMusor";
+import kolesaPoliur from "@/productInfo/kolesa/kolesaPoliur";
+import kolesaBolshPoliur from "@/productInfo/kolesa/kolesaBolshPoliur";
+import kolesaTermNeylon from "@/productInfo/kolesa/kolesaTermNeylon";
+import kolesaTerm from "@/productInfo/kolesa/kolesaTerm";
+import kolesaPoliamid from "@/productInfo/kolesa/kolesaPoliamid";
+import kolesaNeylon from "@/productInfo/kolesa/kolesaNeylon";
+import kolesaAppar from "@/productInfo/kolesa/kolesaAppar";
+import skafSHrOdezhda from "@/productInfo/mebel/skafSHrOdezhda";
+import shkafSHREK from "@/productInfo/mebel/shkafSHREK";
+import shkaf850900 from "@/productInfo/mebel/shkaf850900";
+import SHXA100 from "@/productInfo/mebel/SHXA100";
+import SHMy from "@/productInfo/mebel/SHMy";
+import ALR from "@/productInfo/mebel/ALR";
+import AL from "@/productInfo/mebel/AL";
+import ALS from "@/productInfo/mebel/ALS";
+import TM from "@/productInfo/mebel/TM";
+import SHRS11 from "@/productInfo/mebel/SHRS11";
+import SRHs14 from "@/productInfo/mebel/SRHs14";
+import SHRK22 from "@/productInfo/mebel/SHRK22";
+import SHRK24 from "@/productInfo/mebel/SHRK24";
+import SHrk28 from "@/productInfo/mebel/SHrk28";
+import STF125 from "@/productInfo/stellazhi/STF125";
+import STFL100 from "@/productInfo/stellazhi/STFL100";
+import STFY200 from "@/productInfo/stellazhi/STFY200";
+import MKF350 from "@/productInfo/stellazhi/MKF350";
+import SK125200 from "@/productInfo/stellazhi/SK125200";
+import SGR500 from "@/productInfo/stellazhi/SGR500";
+import kg3500 from "@/productInfo/stellazhi/kg3500";
+import torgov from "@/productInfo/stellazhi/torgov";
+import DSP300 from "@/productInfo/stellazhi/DSP300";
+import DSP400 from "@/productInfo/stellazhi/DSP400";
+import AS25 from "@/productInfo/telega/AS25";
+import KorDlinVilka from "@/productInfo/telega/KorDlinVilka";
+import THR from "@/productInfo/telega/THR";
+import AC3 from "@/productInfo/telega/AC3";
+import JF1000 from "@/productInfo/telega/JF1000";
+import CW25 from "@/productInfo/telega/CW25";
+import platform from "@/productInfo/telega/platform";
+import SHTABgidr from "@/productInfo/telega/SHTABgidr";
+import electroSHTAb from "@/productInfo/telega/electroSHTAb";
+import samoxod from "@/productInfo/telega/samoxod";
+
+export default [
+
+    kolesaProm,kolesaGidr,kolBolGruz,kolBezOpori,
+    kolPnevmo,KolesaMusor,kolesaPoliur,kolesaBolshPoliur,
+    kolesaTermNeylon,kolesaTerm, kolesaPoliamid, kolesaNeylon,
+    kolesaAppar,
+    //mebel
+    skafSHrOdezhda, shkafSHREK, shkaf850900, SHXA100, SHMy, ALR,
+    AL,ALS,TM, SHRS11, SRHs14,SHRK22,SHRK24,SHrk28,
+
+    //stellagi
+    STF125, STFL100,STFY200, MKF350, SK125200, SGR500, kg3500, torgov,
+    DSP300,DSP400,
+
+    //telega
+    AS25,KorDlinVilka, THR, AC3,JF1000,CW25,platform, SHTABgidr,
+    electroSHTAb,samoxod
+
+    //domkrat
+
+
+
+]
