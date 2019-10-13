d5af918a7ab6932daf1da590b27638304e5cd116
grol <grol666@gmail.com>
2019-10-13 18:47:14 +0300

    рефакторинг

--- src/components/navig.vue
+++ src/components/navig.vue
@@ -4,14 +4,19 @@
         <header class="main-navigation">
             <div class="inner-wrap">
                 <div class="inner-nav">
-                    <a  href="indexx.html" class="logo-link"><img src="pics/logo.svg" alt="LOGO" /></a>
-                    <ul class="navigation-list">
-                        <li><router-link to="/catalog">Каталог</router-link></li>
-<!--                        <li><router-link to="/metall-construct">Металлоконструкции</router-link></li>-->
-<!--                        <li><router-link to="/ventellation">Системы вентиляции</router-link></li>-->
-                        <li><router-link to="/about">О нас</router-link></li>
-                        <li><router-link to="/contacts">Контакты</router-link></li>
-                     </ul>
+                    <v-btn-toggle
+                            v-model="toggle_exclusive"
+                            rounded
+                    >
+                        <a  href="indexx.html" class="logo-link"><img src="pics/logo.svg" alt="LOGO" /></a>
+                        <ul class="navigation-list">
+                            <li><router-link to="/catalog">Каталог</router-link></li>
+                            <!--                        <li><router-link to="/metall-construct">Металлоконструкции</router-link></li>-->
+                            <!--                        <li><router-link to="/ventellation">Системы вентиляции</router-link></li>-->
+                            <li><router-link to="/about">О нас</router-link></li>
+                            <li><router-link to="/contacts">Контакты</router-link></li>
+                        </ul>
+                    </v-btn-toggle>
                 </div>
              </div>
             <div class="cross-line">
